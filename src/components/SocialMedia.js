84acf6d5d84b3c325fba27e2944e464cb69ba93c
Khandobin-27 <khandobinworkspace27@gmail.com>
2021-11-02 15:54:09 +0200

    Redirecting to a new tab socials

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -7,9 +7,26 @@ export default function SocialMedia() {
     return (
         <div className='social-media-wrapper'>
             <div className='social-media-container'>
-                <a className='mr' href='https://www.linkedin.com/in/fedor-khandobin-320865158/'><img className='icon-example'  src={linkedin} alt='linkedin-icon'/></a>
-                <a className='mr'href='https://github.com/Khandobin-27'><img className='icon-example'  src={github} alt='github-icon'/></a>
-                <a href="mailto:f.khandobin27@gmail.com"><img className='icon-example'  src={email} alt='email-icon'/></a>
+                <a 
+                target='_blank'
+                rel='noopener noreferrer'
+                className='mr' 
+                href='https://www.linkedin.com/in/fedor-khandobin-320865158/'>
+                <img className='icon-example'  src={linkedin} alt='linkedin-icon'/>
+                </a>
+                <a 
+                target='_blank'
+                rel='noopener noreferrer'
+                className='mr'
+                href='https://github.com/Khandobin-27'>
+                <img className='icon-example'  src={github} alt='github-icon'/>
+                </a>
+                <a 
+                target='_blank'
+                rel='noopener noreferrer'
+                href="mailto:f.khandobin27@gmail.com">
+                <img className='icon-example'  src={email} alt='email-icon'/>
+                </a>
             </div>
             <p className='white-text'>© 2021 - Developed by Fedor Khandobin</p>
         </div>
